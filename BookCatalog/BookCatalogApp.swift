d5b1b83f88461160bd719a661651f786da53bc61
Daniil Khlyshchankou <gfhieemobilelegends@gmail.com>
2025-01-25 18:16:26 +0300

    Initial Commit

---
+++ BookCatalog/BookCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookCatalogApp.swift
+//  BookCatalog
+//
+//  Created by Daniil on 25.01.25.
+//
+
+import SwiftUI
+
+@main
+struct BookCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
